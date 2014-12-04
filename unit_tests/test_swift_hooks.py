8a84922e50b6370c242dddd86ba8269593775b5d
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-12-04 20:58:15 +0000

    improved peer svc stop request logic

--- unit_tests/test_swift_hooks.py
+++ unit_tests/test_swift_hooks.py
@@ -9,26 +9,6 @@ with mock.patch('charmhelpers.core.hookenv.log'):
 
 class SwiftHooksTestCase(unittest.TestCase):
 
-    def test_all_responses_equal(self):
-        responses = [{'a': 1, 'c': 3}]
-        self.assertTrue(swift_hooks.all_responses_equal(responses, 'b',
-                                                        must_exist=False))
-
-        responses = [{'a': 1, 'c': 3}]
-        self.assertFalse(swift_hooks.all_responses_equal(responses, 'b'))
-
-        responses = [{'a': 1, 'b': 2, 'c': 3}]
-        self.assertTrue(swift_hooks.all_responses_equal(responses, 'b'))
-
-        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 1, 'b': 2, 'c': 3}]
-        self.assertTrue(swift_hooks.all_responses_equal(responses, 'b'))
-
-        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 2, 'c': 3}]
-        self.assertTrue(swift_hooks.all_responses_equal(responses, 'b'))
-
-        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 1, 'b': 3, 'c': 3}]
-        self.assertFalse(swift_hooks.all_responses_equal(responses, 'b'))
-
     def test_all_peers_stopped(self):
         token1 = str(uuid.uuid4())
         token2 = str(uuid.uuid4())
@@ -47,18 +27,3 @@ class SwiftHooksTestCase(unittest.TestCase):
                      {'stop-proxy-service-ack': token1},
                      {'some-other-key': token1}]
         self.assertFalse(swift_hooks.all_peers_stopped(responses))
-
-    def test_get_first_available_value(self):
-        rsps = [{'key1': 'A'}, {'key1': 'B'}]
-        self.assertEqual('A',
-                         swift_hooks.get_first_available_value(rsps, 'key1'))
-
-        rsps = [{'key2': 'A'}, {'key1': 'B'}]
-        self.assertEqual('B',
-                         swift_hooks.get_first_available_value(rsps, 'key1'))
-
-        rsps = [{'key2': 'A'}, {'key1': 'B'}]
-        self.assertIsNone(swift_hooks.get_first_available_value(rsps, 'key3'))
-
-        rsps = []
-        self.assertIsNone(swift_hooks.get_first_available_value(rsps, 'key3'))
--- unit_tests/test_swift_utils.py
+++ unit_tests/test_swift_utils.py
@@ -106,3 +106,38 @@ class SwiftUtilsTestCase(unittest.TestCase):
                           'peers-only': None,
                           'stop-proxy-service': None,
                           'stop-proxy-service-ack': None}, rq)
+
+    def test_all_responses_equal(self):
+        responses = [{'a': 1, 'c': 3}]
+        self.assertTrue(swift_utils.all_responses_equal(responses, 'b',
+                                                        must_exist=False))
+
+        responses = [{'a': 1, 'c': 3}]
+        self.assertFalse(swift_utils.all_responses_equal(responses, 'b'))
+
+        responses = [{'a': 1, 'b': 2, 'c': 3}]
+        self.assertTrue(swift_utils.all_responses_equal(responses, 'b'))
+
+        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 1, 'b': 2, 'c': 3}]
+        self.assertTrue(swift_utils.all_responses_equal(responses, 'b'))
+
+        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 2, 'c': 3}]
+        self.assertTrue(swift_utils.all_responses_equal(responses, 'b'))
+
+        responses = [{'a': 1, 'b': 2, 'c': 3}, {'a': 1, 'b': 3, 'c': 3}]
+        self.assertFalse(swift_utils.all_responses_equal(responses, 'b'))
+
+    def test_get_first_available_value(self):
+        rsps = [{'key1': 'A'}, {'key1': 'B'}]
+        self.assertEqual('A',
+                         swift_utils.get_first_available_value(rsps, 'key1'))
+
+        rsps = [{'key2': 'A'}, {'key1': 'B'}]
+        self.assertEqual('B',
+                         swift_utils.get_first_available_value(rsps, 'key1'))
+
+        rsps = [{'key2': 'A'}, {'key1': 'B'}]
+        self.assertIsNone(swift_utils.get_first_available_value(rsps, 'key3'))
+
+        rsps = []
+        self.assertIsNone(swift_utils.get_first_available_value(rsps, 'key3'))
