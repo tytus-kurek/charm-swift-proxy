4d0a18d9d765b0ab4f2edff1e1e1e98b1b94c9e9
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-12-17 23:00:53 +0000

    more

--- unit_tests/test_swift_utils.py
+++ unit_tests/test_swift_utils.py
@@ -21,6 +21,8 @@ def init_ring_paths(tmpdir):
 
 class SwiftUtilsTestCase(unittest.TestCase):
 
+    @mock.patch('swift_utils.rings_synced')
+    @mock.patch('swift_utils.get_broker_token')
     @mock.patch('swift_utils.update_www_rings')
     @mock.patch('swift_utils.get_builders_checksum')
     @mock.patch('swift_utils.get_rings_checksum')
@@ -35,7 +37,10 @@ class SwiftUtilsTestCase(unittest.TestCase):
                           mock_is_elected_leader, mock_path_exists,
                           mock_log, mock_balance_rings,
                           mock_get_rings_checksum,
-                          mock_get_builders_checksum, mock_update_www_rings):
+                          mock_get_builders_checksum, mock_update_www_rings,
+                          mock_get_broker_token, mock_rings_synced):
+        mock_get_broker_token.return_value = "token1"
+        mock_rings_synced.return_value = True
 
         # Make sure same is returned for both so that we don't try to sync
         mock_get_rings_checksum.return_value = None
@@ -73,6 +78,7 @@ class SwiftUtilsTestCase(unittest.TestCase):
         self.assertTrue(mock_set_min_hours.called)
         self.assertTrue(mock_balance_rings.called)
 
+    @mock.patch('swift_utils.get_broker_token')
     @mock.patch('swift_utils.balance_rings')
     @mock.patch('swift_utils.log')
     @mock.patch('swift_utils.is_elected_leader')
@@ -84,7 +90,9 @@ class SwiftUtilsTestCase(unittest.TestCase):
                                                 mock_config,
                                                 mock_is_elected_leader,
                                                 mock_log,
-                                                mock_balance_rings):
+                                                mock_balance_rings,
+                                                mock_get_broker_token):
+        mock_get_broker_token.return_value = "token1"
 
         @swift_utils.sync_builders_and_rings_if_changed
         def mock_balance():
@@ -152,11 +160,9 @@ class SwiftUtilsTestCase(unittest.TestCase):
                           'stop-proxy-service-ack': 'token1',
                           'sync-only-builders': None}, rq)
 
-    @mock.patch('swift_utils.uuid')
-    def test_cluster_rpc_sync_request(self, mock_uuid):
-        mock_uuid.uuid4.return_value = 'token1'
+    def test_cluster_rpc_sync_request(self):
         rpc = swift_utils.SwiftProxyClusterRPC()
-        rq = rpc.sync_rings_request('HostA')
+        rq = rpc.sync_rings_request('HostA', 'token1')
         self.assertEqual({'trigger': 'token1',
                           'builder-broker': 'HostA',
                           'peers-only': None,
