7999d753cfef527e4250c24564f0a58da3c263ae
Edward Hope-Morley <edward.hope-morley@canonical.com>
2015-03-25 10:54:29 -0700

    fix amulet test

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -248,7 +248,6 @@ class SwiftProxyBasicDeployment(OpenStackAmuletDeployment):
             'auth_port': '35357',
             'auth_protocol': 'http',
             'private-address': u.valid_ip,
-            'https_keystone': 'False',
             'auth_host': u.valid_ip,
             'service_username': 'swift',
             'service_tenant_id': u.not_null,
