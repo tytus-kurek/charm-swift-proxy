ccaeae4706dc15561dd7fa16c4e4b8fbaa66aec1
Edward Hope-Morley <edward.hope-morley@canonical.com>
2016-03-02 11:34:58 +0000

    Refactor of ring management code
    Make the ring sync code clearer and improve logic around leader
    switching during or after a sync. Also add more debug logs to
    make it easier to debug when things go wrong.
    
    Closes-Bug: 1448884
    Change-Id: I10d51c74001710b6b7a2502e14370996b15ffb40

--- unit_tests/test_swift_hooks.py
+++ unit_tests/test_swift_hooks.py
@@ -28,32 +28,32 @@ class SwiftHooksTestCase(unittest.TestCase):
 
     @patch("swift_hooks.relation_get")
     @patch("swift_hooks.local_unit")
-    def test_all_peers_stopped(self, mock_local_unit, mock_relation_get):
+    def test_is_all_peers_stopped(self, mock_local_unit, mock_relation_get):
         token1 = str(uuid.uuid4())
         token2 = str(uuid.uuid4())
         mock_relation_get.return_value = token1
 
         responses = [{'some-other-key': token1}]
-        self.assertFalse(swift_hooks.all_peers_stopped(responses))
+        self.assertFalse(swift_hooks.is_all_peers_stopped(responses))
 
         responses = [{'stop-proxy-service-ack': token1},
                      {'stop-proxy-service-ack': token2}]
-        self.assertFalse(swift_hooks.all_peers_stopped(responses))
+        self.assertFalse(swift_hooks.is_all_peers_stopped(responses))
 
         responses = [{'stop-proxy-service-ack': token1},
                      {'stop-proxy-service-ack': token1},
                      {'some-other-key': token1}]
-        self.assertFalse(swift_hooks.all_peers_stopped(responses))
+        self.assertFalse(swift_hooks.is_all_peers_stopped(responses))
 
         responses = [{'stop-proxy-service-ack': token1},
                      {'stop-proxy-service-ack': token1}]
-        self.assertTrue(swift_hooks.all_peers_stopped(responses))
+        self.assertTrue(swift_hooks.is_all_peers_stopped(responses))
 
         mock_relation_get.return_value = token2
 
         responses = [{'stop-proxy-service-ack': token1},
                      {'stop-proxy-service-ack': token1}]
-        self.assertFalse(swift_hooks.all_peers_stopped(responses))
+        self.assertFalse(swift_hooks.is_all_peers_stopped(responses))
 
     @patch.object(swift_hooks, 'config')
     @patch('charmhelpers.contrib.openstack.ip.config')
